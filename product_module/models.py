c5f2ee5fcab076107023523dedb77a2d32fce9b1
Soheil Ghasemi <realsolii778@gmail.com>
2026-01-01 00:47:47 +0330

    Make price field non-nullable
    Set price field to not allow null values.

--- product_module/models.py
+++ product_module/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
     category = models.ManyToManyField(ProductCategory, related_name='product_categories', verbose_name='دسته بندی ها')
     image = models.ImageField(upload_to='images/products', null=True, blank=True, verbose_name='تصویر محصول')
     brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE, verbose_name='برند', null=True, blank=True)
-    price = models.BigIntegerField(verbose_name='قیمت')
+    price = models.BigIntegerField(verbose_name='قیمت',null=False)
     short_description = models.CharField(max_length=360, db_index=True, null=True, verbose_name='توضیحات کوتاه')
     description = models.TextField(verbose_name='توضیحات اصلی', db_index=True)
     slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='عنوان در url')
