1b9f7011e84fafbba6e1c83bd981f81a0bec7b25
Soheil Ghasemi <realsolii778@gmail.com>
2026-01-01 00:45:33 +0330

    Refactor Order model and update OrderDetail count type

--- order_module/models.py
+++ order_module/models.py
@@ -5,21 +5,22 @@ from product_module.models import Product
 
 # Create your models here.
 
-def calculate_total_price(self):
-    total_amount = 0
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    is_paid = models.BooleanField(verbose_name='نهایی شده/نشده')
+    payment_date = models.DateField(null=True, blank=True, verbose_name='تاریخ پرداخت')
 
-    for order_detail in self.orderdetail_set.all():
-        count = order_detail.count or 0
+    def __str__(self):
+        return str(self.user)
 
-        if self.is_paid:
-            price = order_detail.final_price or 0
-        else:
-            price = order_detail.product.price if order_detail.product and order_detail.product.price else 0
+    def calculate_total_price(self):
+        total_amount = 0
 
-        total_amount += price * count
+        for item in self.orderdetail_set.all():
+            price = item.final_price or item.product.price
+            total_amount += price * item.count
 
-    return total_amount
-
+        return total_amount
 
     class Meta:
         verbose_name = 'سبد خرید'
@@ -30,11 +31,13 @@ class OrderDetail(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='سبد خرید')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصول')
     final_price = models.IntegerField(null=True, blank=True, verbose_name='قیمت نهایی تکی محصول')
-    count = models.IntegerField(verbose_name='تعداد')
+    count = models.PositiveIntegerField(verbose_name='تعداد')
 
     def get_total_price(self):
-        return self.count * self.product.price
-
+        # اگر final_price موجود بود از آن استفاده می‌کنیم
+        # در غیر اینصورت قیمت محصول را در نظر می‌گیریم
+        price = self.final_price or (self.product.price if self.product else 0)
+        return price * self.count
     def __str__(self):
         return str(self.order)
 
